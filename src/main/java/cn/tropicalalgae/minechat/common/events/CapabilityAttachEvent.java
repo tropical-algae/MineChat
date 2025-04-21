0ee820c194407883cd1a6c21f953dc0763436315
LINXIAXING <15996160048@163.com>
2025-04-22 05:34:48 +0800

    fix: Change the action scope of CapabilityAttachEvent

--- src/main/java/cn/tropicalalgae/minechat/common/events/CapabilityAttachEvent.java
+++ src/main/java/cn/tropicalalgae/minechat/common/events/CapabilityAttachEvent.java
@@ -4,6 +4,7 @@ package cn.tropicalalgae.minechat.common.events;
 import cn.tropicalalgae.minechat.MineChat;
 import cn.tropicalalgae.minechat.common.capability.TextMemoryProvider;
 import net.minecraft.world.entity.Entity;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,13 +19,15 @@ import static cn.tropicalalgae.minechat.utils.Util.isEntitySupportText;
 public class CapabilityAttachEvent {
     @SubscribeEvent
     public static void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
-        Entity entity = event.getObject();
-        if (isEntitySupportText(entity)) {
-            LOGGER.debug("Init entity memory [TextMessage], target UUID: %s".formatted(entity.getStringUUID()));
-            event.addCapability(
-                    new ResourceLocation(MineChat.MOD_ID, "text_memory"),
-                    new TextMemoryProvider()
-            );
+        if (!event.getObject().level().isClientSide) {
+            Entity entity = event.getObject();
+            if (isEntitySupportText(entity)) {
+                LOGGER.debug("Init entity memory [TextMessage], target UUID: %s".formatted(entity.getUUID().toString()));
+                event.addCapability(
+                        new ResourceLocation(MineChat.MOD_ID, "text_memory"),
+                        new TextMemoryProvider()
+                );
+            }
         }
     }
 }
